7cbb26d9abce093a0f8a8c3bfe8f4306e8327d28
Jay <jaydeepkachare123@gmail.com>
2022-04-16 18:18:00 +0530

    next_distinct function added

--- 08 intersection of sorted array.cpp
+++ 08 intersection of sorted array.cpp
@@ -2,22 +2,24 @@
 using namespace std;
 
 void intersection(int a[], int n1, int b[], int n2);
+int next_distinct(int arr[], int size, int ele);
 
 int main() {
-    int a[] = {3, 5, 10, 10, 15, 15, 20};
-    int b[] = {5, 10, 10, 10, 15, 30};
+    int a[] = {3, 3, 5, 10, 10, 15, 15, 20, 20, 20};
+    int b[] = {3, 3, 5, 10, 10, 10, 15, 18, 20, 20};
 
-    ::intersection(a, 7, b, 6);
+    ::intersection(a, 10, b, 10);
 }
 
 void intersection(int a[], int n1, int b[], int n2) {
     int aptr = 0;
     int bptr = 0;
     while (aptr < n1 && bptr < n2) {
-        if (aptr > 0 && (a[aptr] == b[bptr]) && (a[aptr] != a[aptr - 1])) {
+        if (a[aptr] == b[bptr])
+        {
             cout << a[aptr] << " ";
-            aptr++;
-            bptr++;
+            aptr = ::next_distinct(a, n1, a[aptr]);
+            bptr = ::next_distinct(b, n2, b[bptr]);
         }
         else if (a[aptr] < b[bptr]) {
             aptr++;
@@ -26,4 +28,15 @@ void intersection(int a[], int n1, int b[], int n2) {
         }
     }
     cout << endl;
+}
+
+int next_distinct(int arr[], int size, int ele)
+{
+    int last = -1;
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] == ele)
+            last = i + 1;
+    }
+    return last;
 }
